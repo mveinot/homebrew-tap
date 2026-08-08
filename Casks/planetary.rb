02b21f21ba4fad16076a783f9c3299f33dac497d
Mark Veinot <vic@mvgrafx.net>
2026-08-08 19:54:17 -0300

    planetary 1.9.9.362

--- Casks/planetary.rb
+++ Casks/planetary.rb
@@ -1,14 +1,14 @@
 cask "planetary" do
-  version "1.9.0.351"
+  version "1.9.9.362"
 
   on_arm do
-    sha256 "cd15c2999c51186bf0b70c4463b386df28ad83eeaa8d80b33ddf33483f9aba47"
+    sha256 "bbcc475715608e65676a8d4d00df8003d0196f82b9c5570b700cdc899c9fa260"
 
     url "https://github.com/mveinot/transmission-control/releases/download/v#{version}/Planetary-#{version}-macOS-arm64.dmg",
         verified: "github.com/mveinot/transmission-control/"
   end
   on_intel do
-    sha256 "2dad12ece83f35a2118a6b20246317f3f7d3ed158749c66fbf1151a1b5f2839c"
+    sha256 "1ed8c629e9a57db9d7536fbfe9df8be844428f206f5830248a8d9acbff07aace"
 
     url "https://github.com/mveinot/transmission-control/releases/download/v#{version}/Planetary-#{version}-macOS-x86_64.dmg",
         verified: "github.com/mveinot/transmission-control/"
